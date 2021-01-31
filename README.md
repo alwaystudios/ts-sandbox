df97369abcd0120f7213d00733f2fa176e575068
Gary Alway <garyalway@gmail.com>
2021-01-31 11:35:34 +0000

    deep partial

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## links
 
 - tsdef - https://github.com/joonhocho/tsdef/blob/master/src/index.ts
+- advanced types - https://www.typescriptlang.org/docs/handbook/advanced-types.html
---
+++ src/deepPartial.spec.ts
@@ -0,0 +1,33 @@
+import { dissoc } from 'ramda'
+
+type DeepPartial<T> = { [K in keyof T]?: DeepPartial<T[K]> }
+
+type Data = {
+  key1: string
+  deepKey: {
+    key2: number
+    optionalDeepKey?: {
+      key3: number
+    }
+  }
+}
+
+test('deep partial', () => {
+  const data: Data = {
+    key1: 'test 1',
+    deepKey: {
+      key2: 1,
+    },
+  }
+
+  const partialData: DeepPartial<Data> = {}
+
+  expect(data).not.toEqual(partialData)
+
+  const partialData2: DeepPartial<Data> = { ...data }
+  expect(data).toEqual(partialData2)
+
+  const partialData3: DeepPartial<Data> = { key1: 'test 1', deepKey: { optionalDeepKey: {} } }
+  expect(data).toMatchObject(dissoc<Data>('deepKey', partialData3))
+  expect(data).not.toMatchObject({ ...partialData3, someOtherKey: 22 })
+})
