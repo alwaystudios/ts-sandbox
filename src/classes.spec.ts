3661db6f53d196a3a649777e5abd201d3d4b9324
Gary Alway <garyalway@gmail.com>
2021-01-31 22:51:18 +0000

    simple class with static async init eg

---
+++ src/classes.spec.ts
@@ -0,0 +1,47 @@
+const fetchUser = jest.fn().mockResolvedValue({ userId: '123', username: 'test 1' })
+const fetchUserPosts = jest
+  .fn()
+  .mockResolvedValue([
+    { title: 'my first post', content: 'this is my first post', timestamp: Date.now() },
+  ])
+
+interface IUserPost {
+  getUsername: () => string
+}
+
+type UserInfo = {
+  userId: string
+  username: string
+}
+
+type Post = {
+  title: string
+  content: string
+  timestamp: number
+}
+
+class UserPost implements IUserPost {
+  user: UserInfo
+  posts: Post[]
+
+  constructor(user: UserInfo, posts: Post[]) {
+    this.user = user
+    this.posts = posts
+  }
+
+  static async init(userId: string): Promise<UserPost> {
+    const [user, posts] = await Promise.all([fetchUser(userId), fetchUserPosts(userId)])
+
+    return new UserPost(user, posts)
+  }
+
+  getUsername() {
+    return this.user.username
+  }
+}
+
+test('get username', async () => {
+  const user1 = await UserPost.init('123')
+
+  expect(user1.getUsername()).toEqual('test 1')
+})
