5adc875d56c8563dc326b167cc7b7e4080c0f946
Gary Alway <garyalway@gmail.com>
2021-08-30 21:54:18 +0100

    minor refactor

--- src/interview.spec.ts
+++ src/interview.spec.ts
@@ -1,12 +1,8 @@
-const calculate = (arr: number[]) => {
-  const numbersWithDuplicates = Array.from(new Set(arr)).reduce(
-    (acc: number[], curr: number) =>
-      arr.filter((x) => curr === x).length > 1 ? [...acc, curr] : acc,
-    [],
+const calculate = (arr: number[]) =>
+  arr.reduce(
+    (acc: number, curr: number) => (arr.filter((x) => curr === x).length > 1 ? acc + curr : acc),
+    0,
   )
-
-  return arr.filter((n) => numbersWithDuplicates.includes(n)).reduce((acc, curr) => acc + curr, 0)
-}
 
 test('sums all numbers in an array that appear more than once', () => {
   expect(calculate([7, 2, 7, 7, 9, 5, 2])).toEqual(25)
